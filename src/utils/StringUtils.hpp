08cf2e61ff2d7ab34111da188ee87b58fd79340d
James Liu <jamesjhliu@gmail.com>
2018-02-07 14:35:41 -0800

    add trim to stringutils

--- src/utils/StringUtils.hpp
+++ src/utils/StringUtils.hpp
@@ -5,5 +5,10 @@
 #include <iterator>
 #include <sstream>
 
-template<typename Out>
-void str_split(Out result, const std::string &s, char delim);
+void ltrim(std::string & s);
+
+void rtrim(std::string & s);
+
+void trim(std::string & s);
+
+std::vector<std::string> split(const std::string & s, char delim);
