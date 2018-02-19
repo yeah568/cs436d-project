878a8f1072c26eac352a02b8738eb229550c2faf
Scott Luu <scottluu95@gmail.com>
2018-02-19 14:49:15 -0800

    Fix Travis Attempt 2

--- src/common.cpp
+++ src/common.cpp
@@ -92,7 +92,7 @@ vec2 operator*(const float lhs, const vec2& rhs) {
 	return {rhs.x * lhs, rhs.y * lhs};
 }
 vec2 rotate(const vec2& vector, const float angle) {
-	return {(float) vector.x*cos(angle) - vector.y*sin(angle), (float) vector.x*sin(angle) + vector.y*cos(angle)};
+	return {static_cast<float> (vector.x*cos(angle) - vector.y*sin(angle)), static_cast<float> (vector.x*sin(angle) + vector.y*cos(angle))};
 }
 vec2 normalize(vec2 v)
 {
