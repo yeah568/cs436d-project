39ff3eed437581dac973fb74f4cb85add4aa4ad6
t_ott <tvrot@hotmail.com>
2018-02-27 00:33:41 -0800

    Fix crash

--- src/world.cpp
+++ src/world.cpp
@@ -75,12 +75,12 @@ bool World::init(vec2 screen)
 	glfwSetCursorPosCallback(m_window, cursor_pos_redirect);
 
 	//return levelList[levelCounter].init
-	Level1* level = new Level1(screen.x, screen.y);
-	//levelList.emplace_back(new Level1(screen.x, screen.y));
+	//Level1* level = new Level1(screen.x, screen.y);
+	levelList.emplace_back(new Level1(screen.x, screen.y));
 	//levelList.emplace_back(new Level2(screen.x, screen.y));
 	//levelList.emplace_back(new Level1(screen.x, screen.y, 3));
 	
-	return level->init();
+	return levelList[levelCounter]->init();
 }
 
 // Releases all the associated resources
