577911f21184e379c35a94973a74a3b78f7f83a1
Scott Luu <scottluu95@gmail.com>
2018-03-06 16:34:49 -0800

    stopped player from getting more than 5 health

--- src/Player.hpp
+++ src/Player.hpp
@@ -51,7 +51,7 @@ public:
 
 	bool bullet_type;
 
-	void set_health(float delta) {m_health += delta;};
+	void set_health(float delta) {m_health += std::min(5-get_health(),delta);};
 
 	float get_health() {return m_health;};
 
